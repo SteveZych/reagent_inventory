1bab345b4f8a7393a2903ef2d3ce2fa646615079
Steven Zych <stevezych@gmail.com>
2021-06-05 10:14:10 -0600

    Updates

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 
+#Create an instance of Flask
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+
+#Establish mongo connection
+mongo = PyMongo(app, url="mongodb://localhost:27017/reagent_inventory")
+
+#Route to render index.html
+
 
