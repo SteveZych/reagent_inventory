1f2608b77110f9bdc6773cad02ba6839c2e21d50
Steven Zych <stevezych@gmail.com>
2021-06-09 19:38:32 -0600

    added input on add.html, added form variables on app.py

--- app.py
+++ app.py
@@ -5,8 +5,20 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 #Establish mongo connection
-mongo = PyMongo(app, url="mongodb://localhost:27017/reagent_inventory")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/reagent_inventory"
+mongo = PyMongo(app)
+
 
 #Route to render index.html
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/add")
+def add():
+    if request.method == "POST":
+        
+    return render_template('app.html')
+
 
 
