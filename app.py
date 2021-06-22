c6d4928d8009eeb02dae382dc760069624252044
Steven Zych <stevezych@gmail.com>
2021-06-21 21:58:07 -0600

    added correct method to post from add page. Started creating class for reagents

--- app.py
+++ app.py
@@ -8,14 +8,27 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/reagent_inventory"
 mongo = PyMongo(app)
 
+# Class to initalize new inventory
+class Reagent():
+    def __init__(self, section, reagent, lot, expiration, receive, quantity):
+        self.section = section
+        self.reagent = reagent
+        self.lot = lot
+        self.expiration = expiration
+        self.receive = receive
+        self.quantity = quantity
+
+    
+
+
 #Route to render index.html
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
-@app.route("/add", methods=['POST'])
+@app.route("/add", methods=['GET','POST'])
 def add():
-    if request.method == "POST":
+    if request.method == 'POST':
         section = request.form['section']
         reagent = request.form['reagent']
         lot = request.form['lot']
@@ -23,23 +36,28 @@ def add():
         receive = request.form['receive']
         quantity = request.form['quantity']
 
-        add = {
-            'section': section,
-            'reagent': reagent,
-            'lot': lot,
-            'expiration': expiration,
-            'received': receive,
-            'quantity': quantity
-        }
-
-        try:
-            mongo.db.reagents.insert_one(add, upsert=True)
-            return redirect('/index', code=302)
-        except:
-            return "There Was a problem adding."
-    
+        print(section, reagent, lot, expiration, receive, quantity)
+        return redirect('/')
     else:
-        return render_template("index0.html")
+        return render_template('add.html')
+
+    #     add = {
+    #         'section': section,
+    #         'reagent': reagent,
+    #         'lot': lot,
+    #         'expiration': expiration,
+    #         'received': receive,
+    #         'quantity': quantity
+    #     }
+
+    #     try:
+    #         mongo.db.reagents.insert_one(add, upsert=True)
+    #         return redirect('/index', code=302)
+    #     except:
+    #         return "There Was a problem adding."
+    
+    # else:
+    #     return render_template("index.html")
 
     
 if __name__ == "__main__":
